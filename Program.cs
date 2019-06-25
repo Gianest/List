bd4135dbe1be69de7bd3e957c80a7e0212df5695
AEWR <rtgsdg@WTAAEWR>
2019-06-25 12:14:42 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lstnamaMhs = new List<string>();
+            lstnamaMhs.Add("erick");
+            lstnamaMhs.Add("wely");
+            lstnamaMhs.Add("rully");
+
+            string namaMhs = lstnamaMhs[1];
+
+            Console.WriteLine(namaMhs);
+            Console.WriteLine();
+
+            foreach (string nama in lstnamaMhs)
+            {
+                Console.WriteLine(nama);
+            }
+            Console.ReadKey();
+        }
+    }
+}
